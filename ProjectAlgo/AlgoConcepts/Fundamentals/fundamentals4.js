8847da608acc20c925575384d126a9e06e1d7176
Phone Myat <hanminsoe10@gmail.com>
2024-02-04 23:51:13 -0600

    continue doing ass

--- ProjectAlgo/AlgoConcepts/Fundamentals/fundamentals4.js
+++ ProjectAlgo/AlgoConcepts/Fundamentals/fundamentals4.js
@@ -20,3 +20,7 @@ console.log(onlyKeepTheLastFew(arr,val))
 
 // Math Help
 // Cartman doesn’t really like math; he needs help. You are given two numbers – coefficients M and B in the equation Y = MX + B. Build a function to return the X-intercept (his older cousin Fiaz wisely reminds him that X-intercept is the value of X where Y equals zero; Cartman just snorts in his general direction).
+
+// function mathHelp (M,B){
+//     M  == 0?  "Its a horizonal line" : -B/M
+// }
